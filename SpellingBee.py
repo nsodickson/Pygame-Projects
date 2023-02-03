637f8064a22b1d48f745647545047284d9309a48
Noah Sodickson <nsodickson@gmail.com>
2023-02-02 21:09:36 -0500

    Better letter generation

--- SpellingBee.py
+++ SpellingBee.py
@@ -1,12 +1,11 @@
 import pygame
 from pygame.locals import *
-import string
 import random
 import math
 
 # Initializing Pygame and Pygame Window
 pygame.init()
-w, h = 500, 500
+w, h = 1000, 500
 win = pygame.display.set_mode((w, h))
 font = pygame.font.Font(None, 50)
 large_font = pygame.font.Font(None, 60)
@@ -30,7 +29,7 @@ circles = [(250, 250),
            (175, 300),
            (175, 200)]
 circle_radius = 40
-button = pygame.Rect(350, 15, 125, 30)
+reset_button = pygame.Rect(350, 15, 125, 30)
 
 # Integer and Boolean flags for multi-frame actions
 delete = False
@@ -42,10 +41,26 @@ message = ""
 def dist(p1, p2):
     return math.sqrt((p2[1] - p1[1]) ** 2 + (p2[0] - p1[0]) ** 2)
 
-def genLetters(key, num_letters):
+# Randomly samples letters from a key
+def genRandomLetters(key, num_letters):
     assert num_letters < len(key)
     return random.sample(key, num_letters)
 
+# Randomly samples a word with a given number of unqiue letters from a key, shuffles the letters, and returns them
+def genLetters(word_key, num_letters):
+    word_key_shuffled = random.sample(word_key, len(word_key))
+    for word in word_key_shuffled:
+        letters = []
+        for letter in word:
+            if letter not in letters:
+                letters.append(letter)
+        if len(letters) == num_letters:
+            random.shuffle(letters)
+            return letters
+    print("No letters found")
+    return []
+
+# Checks if a queue of letters contains every letter in a key at least once
 def isPanogram(key, letter_queue):
     for letter in key:
         if letter not in letter_queue:
@@ -53,16 +68,15 @@ def isPanogram(key, letter_queue):
     return True
 
 # Obtain master word and letter list
-with open("words_alpha.txt") as f:
-    words = f.read().split("\n")
+with open("words_large.txt") as f:
+    word_key = f.read().split("\n")
 vowels = "aeiou"
 consonants = "bcdfghjklmnpqrstvwxyz"
 
 while True:
     gameOn = True
     
-    letters = genLetters(vowels, 3) + genLetters(consonants, 4)
-    random.shuffle(letters)
+    letters = genLetters(word_key, 7)
     
     letter_queue = ""
     word_queue = []
@@ -77,12 +91,15 @@ while True:
             elif event.type == TEXTINPUT:
                 if event.text.lower() in letters:
                     letter_queue += event.text.lower()
-                    
+            
             elif event.type == KEYDOWN:
+                # Handling deleting letters
                 if event.key == K_BACKSPACE:
                     letter_queue = letter_queue[0:-1]
                     delete = True
                     delete_timer = 10
+                
+                # Handling checking a word
                 elif event.key == K_RETURN:
                     if letter_queue in word_queue:
                         message = "Already Found"
@@ -90,10 +107,10 @@ while True:
                         message = "Missing Center Letter"
                     elif len(letter_queue) < 4:
                         message = "Too Short"
-                    elif letter_queue in words:
+                    elif letter_queue in word_key:
                         word_queue.append(letter_queue)
                         if isPanogram(letters, letter_queue):
-                            message = "Panogram!"
+                            message = "Pangram!"
                             score += 5 * len(letter_queue)
                         else:
                             message = "+"
@@ -102,16 +119,18 @@ while True:
                         message = "Not a Word"
                     display_timer = 10
                     letter_queue = ""
-                    
+            
+            # Handling stopping deleting letters
             elif event.type == KEYUP:
                 if event.key == K_BACKSPACE:
                     delete = False
             
+            # Handling clicking the new game button or a letter
             elif event.type == MOUSEBUTTONDOWN:
                 for letter, center in zip(letters, circles):
                     if dist(center, event.pos) < circle_radius:
                         letter_queue += letter
-                if button.collidepoint(event.pos):
+                if reset_button.collidepoint(event.pos):
                     gameOn = False
         
         win.fill(bg_color)
@@ -121,26 +140,43 @@ while True:
                 delete_timer -= 1
             else:
                 letter_queue = letter_queue[0:-1]
-                
-        pygame.draw.rect(win, poly_color, button)
+        
+        # Drawing the new game button
+        pygame.draw.rect(win, poly_color, reset_button)
         button_surf = small_font.render("New Game", True, font_color)   
         button_size = small_font.size("New Game")
         win.blit(button_surf, (350 + (125 - button_size[0]) / 2, 15 + button_size[1] / 2))
-                
+        
+        # Drawing the score
         score_surf = font.render(f"Score: {score}", True, font_color)
         win.blit(score_surf, (15, 15))
         
+        # Drawing the currently guessed letters
         letter_queue_surf = font.render(letter_queue, True, font_color)
         letter_queue_size = font.size(letter_queue)
         win.blit(letter_queue_surf, (250 - letter_queue_size[0] / 2, 50))
         
+        # Drawing the letters
         for letter, center in zip(letters, circles):
             pygame.draw.circle(win, poly_color, center, circle_radius)
             letter_surf = font.render(letter, True, font_color)
             letter_size = font.size(letter)
             win.blit(letter_surf, (center[0] - letter_size[0] / 2, center[1] - letter_size[1] / 2))
-         
-         # Code to display a message after entering a word
+        
+        # Drawing the already guessed words
+        x, y, max_length = 0, 0, 0
+        for idx, word in enumerate(word_queue):
+            max_length = max(small_font.size(word)[0], max_length)
+            word_surf = small_font.render(word, False, font_color)
+            win.blit(word_surf, (x + 500, y))
+            if y >= 450:
+                x += max_length + 15
+                y = 0
+                max_length = 0
+            else:
+                y += 30
+
+         # Drawing the message after checking a word
         if display_timer > 0:
             message_surf = large_font.render(message, False, font_color)
             message_surf.set_alpha(255 - 25.5 * (10 - display_timer))
