73a125e5c8e88459883e66bca3eee18e4ad481dc
Noah Sodickson <nsodickson@gmail.com>
2024-06-21 13:04:27 -0400

    Optimized Gravity

--- Games/FlappyBird.py
+++ Games/FlappyBird.py
@@ -5,6 +5,12 @@ import random
 pygame.init()
 
 
+def map_range(in_min, in_max, out_min, out_max, val):
+    in_range = in_max - in_min
+    out_range = out_max - out_min
+    return (val - in_min) * out_range / in_range + out_min
+
+
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
         super(Bird, self).__init__()
@@ -14,13 +20,18 @@ class Bird(pygame.sprite.Sprite):
         self.w = w
         self.h = h
 
-        self.image = pygame.image.load("Assets/flappy.png")
-        self.image = pygame.transform.scale(self.image, (w, h))
+        self.base_image = pygame.image.load("Assets/flappy.png")
+        self.base_image = pygame.transform.scale(self.base_image, (w, h))
+        self.image = self.base_image.copy()
         self.rect = pygame.Rect(x, y, w, h)
+        self.mask = pygame.mask.from_surface(self.image)
     
     def update(self):
         self.pos += self.vel
         self.rect.update(self.pos.x, self.pos.y, self.w, self.h)
+        clamped_vel = pygame.math.clamp(self.vel.y, 0, 2)
+        self.image = pygame.transform.rotate(self.base_image, map_range(0, 2, 20, -45, clamped_vel))
+        self.mask = pygame.mask.from_surface(self.image)
     
     def draw(self, window):
         window.blit(self.image, self.pos)
@@ -34,9 +45,17 @@ class TopPipe(pygame.sprite.Sprite):
         self.vel = pygame.Vector2(-0.5, 0)
         self.h = h
 
-        self.image = pygame.image.load("Assets/pipe-top.png")
-        self.image = pygame.transform.scale(self.image, (80, h))
+        pipe_base = pygame.image.load("Assets/pipe-base-top.png")
+        pipe_top = pygame.image.load("Assets/pipe-top.png")
+        scale = 80 / pipe_top.get_width()
+        pipe_top = pygame.transform.scale_by(pipe_top, scale)
+        pipe_base = pygame.transform.scale(pipe_base, (80, h - pipe_top.get_height()))
+        self.image = pygame.Surface((80, h), flags=SRCALPHA)
+        self.image.fill((255, 255, 255, 0))
+        self.image.blit(pipe_base, (0, 0))
+        self.image.blit(pipe_top, (0, pipe_base.get_height()))
         self.rect = pygame.Rect(x, y, 80, h)
+        self.mask = pygame.mask.from_surface(self.image)
     
     def update(self):
         self.pos += self.vel
@@ -51,66 +70,102 @@ class BottomPipe(pygame.sprite.Sprite):
         self.vel = pygame.Vector2(-0.5, 0)
         self.h = h
 
-        self.image = pygame.image.load("Assets/pipe-bottom.png")
-        self.image = pygame.transform.scale(self.image, (80, h))
+        pipe_base = pygame.image.load("Assets/pipe-base-bottom.png")
+        pipe_bottom = pygame.image.load("Assets/pipe-bottom.png")
+        scale = 80 / pipe_bottom.get_width()
+        pipe_bottom = pygame.transform.scale_by(pipe_bottom, scale)
+        pipe_base = pygame.transform.scale(pipe_base, (80, h - pipe_bottom.get_height()))
+        self.image = pygame.Surface((80, h), flags=SRCALPHA)
+        self.image.fill((255, 255, 255, 0))
+        self.image.blit(pipe_bottom, (0, 0))
+        self.image.blit(pipe_base, (0, pipe_bottom.get_height()))
         self.rect = pygame.Rect(x, y, 80, h)
+        self.mask = pygame.mask.from_surface(self.image)
     
     def update(self):
         self.pos += self.vel
         self.rect.update(self.pos.x, self.pos.y, 80, self.h)
 
-    
 width, height = 1200, 800
 window = pygame.display.set_mode((width, height))
 bg_color = (173, 216, 230)
-bird = Bird(200, 400, 50, 30)
-pipes = pygame.sprite.Group()
-
+font_big = pygame.font.Font(None, 80)
+font_med = pygame.font.Font(None, 60)
 clock = pygame.time.Clock()
 pipe_delay = 2
-ticks = 0
 fps = 500
-
 gravity = 0.01
+max_score = 0
 
-game_on = True
-while game_on:
-    window.fill(bg_color)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            quit()
-        elif event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                bird.vel.y = -1
-    
-    if ticks / fps % pipe_delay == 0:
-        h = random.randint(200, 600)
-        pipes.add(TopPipe(1200, 0, h), BottomPipe(1200, h + 150, height - h - 150))
+def run():
+    global max_score
 
-    bird.vel.y += gravity
-    bird.update()
-    pipes.update()
+    score = 0
+    ticks = 0
+    bird = Bird(200, 400, 50, 30)
+    pipes = pygame.sprite.Group()
 
-    if bird.pos.y + bird.h > height:
-        game_on = False
-    elif bird.pos.y < 0:
-        game_on = False 
-    
-    for pipe in pipes:
-        if pygame.sprite.collide_rect(bird, pipe):
+    game_on = True
+    while game_on:
+        window.fill(bg_color)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit()
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    bird.vel.y = -1
+        
+        if ticks / fps % pipe_delay == 0:
+            h = random.randint(200, 600)
+            pipes.add(TopPipe(1200, 0, h), BottomPipe(1200, h + 100, height - h - 100))
+
+        bird.vel.y += gravity
+        bird.update()
+        pipes.update()
+
+        if bird.pos.y + bird.h > height:
             game_on = False
+        elif bird.pos.y < 0:
+            game_on = False
+        
+        for pipe in pipes:
+            if pygame.sprite.collide_mask(bird, pipe) is not None:
+                game_on = False
+            elif isinstance(pipe, BottomPipe) and pipe.pos.x + 40 == bird.pos.x:
+                score += 1
+                if score > max_score:
+                    max_score = score
 
-    for pipe in pipes:
-        if pipe.pos.x + 80 < 0:
-            pipes.remove(pipe)
+        for pipe in pipes:
+            if pipe.pos.x + 80 < 0:
+                pipes.remove(pipe)
 
-    bird.draw(window)
-    pipes.draw(window)
-    pygame.display.update()
-    clock.tick(fps)
-    ticks += 1
+        bird.draw(window)
+        pipes.draw(window)
+        font_surf = font_big.render(str(score), True, (255, 0, 0))
+        window.blit(font_surf, (25, 25))
+        pygame.display.update()
+        clock.tick(fps)
+        ticks += 1
 
+run()
+
+restart = font_big.render("Click to Restart", True, (255, 0, 0))
+restart_size = font_big.size("Click to Restart")
+restart_pos = (width / 2 - restart_size[0] / 2, height / 2 - restart_size[1] / 2)
+restart_rect = pygame.Rect(restart_pos[0], restart_pos[1], restart_size[0], restart_size[1])
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             quit()
+        elif event.type == MOUSEBUTTONDOWN:
+            if restart_rect.collidepoint(event.pos):
+                run()
+    
+    highscore = font_med.render("Highest: {}".format(max_score), True, (255, 0, 0))
+    highscore_size = font_med.size("Highest: {}".format(max_score))
+    highscore_pos = (width / 2 - highscore_size[0] / 2, restart_pos[1] + restart_size[1] + 5)
+    window.blit(highscore, highscore_pos)
+    window.blit(restart, restart_pos)
+    pygame.display.update()
+    
--- Games/GravitySimTwo.py
+++ Games/GravitySimTwo.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import math
 import random
+import time
 
 pygame.init()
 
@@ -29,33 +30,99 @@ class Ball(pygame.sprite.Sprite):
         
         self.clicked = False
     
-    def update(self):
-        self.pos += self.vel
-        self.rect.update(self.pos.x - self.radius, self.pos.y - self.radius, self.radius * 2, self.radius * 2)
-    
     def setPos(self, pos):
         self.pos = pos
         self.rect.update(pos.x - self.radius, pos.y - self.radius, self.radius * 2, self.radius * 2)
     
+    def setRadius(self, radius):
+        self.radius = radius
+        self.image = pygame.Surface((radius * 2, radius * 2), flags=SRCALPHA)
+        self.image.fill((255, 255, 255, 0))
+        pygame.draw.circle(self.image, (255, 0, 0), (radius, radius), radius)
+        self.rect = pygame.Rect(self.pos.x - radius, self.pos.y - radius, radius * 2, radius * 2)
+    
+    def setMass(self, mass):
+        self.mass = mass
+    
+    def update(self):
+        self.pos += self.vel
+        self.rect.update(self.pos.x - self.radius, self.pos.y - self.radius, self.radius * 2, self.radius * 2)
+    
     def stop(self):
         self.vel = pygame.Vector2(0, 0)
 
+
+def handleMerges():
+    global balls
+    centers = sorted(balls.sprites(), key=lambda x: -x.mass)
+    satellites = sorted(balls.sprites(), key=lambda x: -x.mass)
+    i = 0
+    while i < len(centers):
+        n = i + 1
+        while n < len(satellites):
+            center = centers[i]
+            satellite = satellites[n]
+            radius = dist(center.pos, satellite.pos)
+            if radius < center.radius + satellite.radius:
+                center.setMass(center.mass + satellite.mass)
+                center.setRadius(math.sqrt(center.radius ** 2 + satellite.radius ** 2))
+                center.vel = (center.mass * center.vel + satellite.mass * satellite.vel) / (center.mass + satellite.mass)
+                balls.remove(satellite)
+                satellites.remove(satellite)
+                centers.remove(satellite)
+            n += 1
+        i += 1
+
+def handleGravity():
+    centers = balls.sprites()
+    satellites = balls.sprites()
+    for i in range(len(centers)):
+        for n in range(i + 1, len(satellites)):
+            center = centers[i]
+            satellite = satellites[n]
+            radius = dist(center.pos, satellite.pos)
+            
+            mag1 = G * center.mass / radius ** 2
+            dir1 = center.pos - satellite.pos
+            dir1.scale_to_length(mag1)
+            satellite.vel += dir1
+            satellite.vel.scale_to_length(min(C, satellite.vel.magnitude()))
+
+            mag2 = G * satellite.mass / radius ** 2
+            dir2 = satellite.pos - center.pos
+            dir2.scale_to_length(mag2)
+            center.vel += dir2
+            center.vel.scale_to_length(min(C, center.vel.magnitude()))
+
 width, height = 800, 800
 window = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+fps = 100
+paused = False
+gravity_on = True
+past_pos = pygame.Vector2(0, 0)
+
+G = 1
+C = 100000000
+
 balls = pygame.sprite.Group()
 
 star = Ball(400, 400, 10000, 50)
-planet1 = Ball(600, 400, 100, 10)
-planet2 = Ball(200, 400, 1000, 25)
-planet1.vel = (random.randint(-10, 10), random.randint(-10, 10))
-planet2.vel = (random.randint(-10, 10), random.randint(-10, 10))
-balls.add(star, planet1)
+randomness = 0
+planets = []
+for i in range(10000):
+    r = random.random() * (width - star.radius) + star.radius
+    theta = random.random() * 2 * math.pi
+    planet = Ball(r * math.cos(theta) + width / 2, r * math.sin(theta) + height / 2, 1, 1)
+    vel = (planet.pos - star.pos).rotate(90)
+    vel.scale_to_length(min(C, math.sqrt(G * star.mass / dist(planet.pos, star.pos)) + random.randint(-randomness, randomness)))
+    planet.vel = vel
+    planets.append(planet)
 
-clock = pygame.time.Clock()
-paused = False
-past_pos = pygame.Vector2(0, 0)
+balls.add(star, *planets)
 
-gravity = 1
+merge_times = []
+gravity_times = []
 
 game_on = True
 while game_on:
@@ -85,18 +152,36 @@ while game_on:
                 for ball in balls:
                     ball.setPos(ball.pos + tupleToVector2(event.pos) - past_pos)
             past_pos = tupleToVector2(event.pos)
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                fps = 25
+            elif event.key == K_RIGHT:
+                fps = 500
+            elif event.key == K_SPACE:
+                gravity_on = not gravity_on
+        elif event.type == KEYUP:
+            if event.key == K_LEFT or event.key == K_RIGHT:
+                fps = 100
     
     if not paused:
-        for center in balls:
-            for satellite in balls:
-                radius = dist(center.pos, satellite.pos)
-                if abs(radius) < center.radius: continue
-                mag = gravity * center.mass / radius ** 2
-                dir = center.pos - satellite.pos
-                dir.scale_to_length(mag)
-                satellite.vel += dir
+
+        start = time.perf_counter()
+        handleMerges()
+        merge_times.append(time.perf_counter() - start)
+
+        if gravity_on:
+            start = time.perf_counter()
+            handleGravity()
+            gravity_times.append(time.perf_counter() - start)
+            
         balls.update()
+
+    # Drawing the Schwarzschild Radius of the star
+    # pygame.draw.circle(window, (50, 50, 50, 0.5), star.pos, 2 * G * star.mass / C ** 2, width=5)
 
     balls.draw(window)
     pygame.display.update()
-    clock.tick(100)
+    clock.tick(fps)
+
+print("Average Merge Time: {}".format(sum(merge_times) / len(merge_times)))
+print("Average Gravity Time: {}".format(sum(gravity_times) / len(gravity_times)))
